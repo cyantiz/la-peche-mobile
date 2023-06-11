0224622ed146d7299eb26943eadf5bd02bc890f5
nhan <nhan@arckipel.com>
2023-06-11 12:01:47 +0700

    config: capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+    appId: 'la.peche.ionic',
+    appName: 'la-peche-ionic',
+    webDir: '.output/public',
+    server: {
+        androidScheme: 'https',
+    },
+}
+
+export default config
